50fa7df4e81cba41d68fa7d46204c4b9352ae5f7
Samuel Gonzalez <samtristan11@gmail.com>
2025-12-04 13:20:48 -0800

    Change home route from '/home' to '/'

--- webiste/views.py
+++ webiste/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__, template_folder='template')
 
-@views.route('/home')
+@views.route('/')
 def home():
     return render_template('home.html')
 
