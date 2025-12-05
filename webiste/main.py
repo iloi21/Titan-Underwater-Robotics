0d200b3d80e1c75facc882d64428f823f285c086
Samuel Gonzalez <samtristan11@gmail.com>
2025-12-04 23:43:20 -0800

    added code to make port forwarding run

--- webiste/main.py
+++ webiste/main.py
@@ -6,4 +6,4 @@ app.register_blueprint(views, url_prefix='/')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
--- webiste/views.py
+++ webiste/views.py
@@ -1,8 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 views = Blueprint('views', __name__, template_folder='template')
 
 @views.route('/')
+def index():
+    return redirect(url_for('views.home'))
+
+@views.route('/home')
 def home():
     return render_template('home.html')
 
